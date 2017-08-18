c3ef8f8cd2f9b744b238c06b746870514df40ca0
ddemingjr22 <ddemingjr22@yahoo.com>
2017-08-18 18:56:13 -0400

    added new code

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,6 +10,29 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            //first try
+            int costTotal = 120;
+            if(costTotal >= 50)
+            {
+                Console.WriteLine("Congratulations your bill total qualifies for a 10% discount!");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations your bill total qualifies for a 5% discount!");
+            }
+
+            //Lets try this one out
+
+
+
+
+
+
+
+
+
+
         }
+
     }
 }
